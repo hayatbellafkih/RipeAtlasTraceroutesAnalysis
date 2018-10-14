1bcab94a38db1c3cfc062435c6d6b601b1480689
hayatbellafkih <belafkih.hayat@gmail.com>
2018-10-14 18:55:51 +0200

    mise à jour du rapport memoire.pdf

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # RipeAtlasTraceroutesAnalysis
 
-Dossier : rapport_memoire 
+#rapport_memoire 
 Le rapport du mémoire, voir memoire.pdf. 
+
+#tableaux
+taille_distributions_medianes.pdf : tableau représentant la taille de la distribution  des médianes par lien.
---
+++ maps/traceroute_attributes.mm
@@ -0,0 +1,245 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT=" traceroute" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1539524937133"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12"/>
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<node TEXT=" af" POSITION="right" ID="ID_257547382" CREATED="1539519304518" MODIFIED="1539524937138">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="dst_addr" POSITION="right" ID="ID_544959028" CREATED="1539519390168" MODIFIED="1539524937139">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="bundle" POSITION="right" ID="ID_821896257" CREATED="1539519322771" MODIFIED="1539524937140">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="dst_name" POSITION="right" ID="ID_815388911" CREATED="1539519411930" MODIFIED="1539524937141">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="fw" POSITION="right" ID="ID_1514634841" CREATED="1539519426932" MODIFIED="1539524937142">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="endtime" POSITION="right" ID="ID_789089905" CREATED="1539519439313" MODIFIED="1539524937143">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="from" POSITION="right" ID="ID_201685410" CREATED="1539519444512" MODIFIED="1539524937144" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="group_id" POSITION="right" ID="ID_1237765220" CREATED="1539519511819" MODIFIED="1539524937145">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="lts" POSITION="right" ID="ID_1409511695" CREATED="1539519517202" MODIFIED="1539524937146">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="msm_id" POSITION="right" ID="ID_754805992" CREATED="1539519523062" MODIFIED="1539524937147" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="msm_name" POSITION="right" ID="ID_1023883798" CREATED="1539519528018" MODIFIED="1539524937148">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="paris_id" POSITION="right" ID="ID_956820708" CREATED="1539519595428" MODIFIED="1539524937148">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="prb_id" POSITION="right" ID="ID_1175684803" CREATED="1539519612625" MODIFIED="1539524937149" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="proto" POSITION="right" ID="ID_1675162394" CREATED="1539519617986" MODIFIED="1539524937150">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="size" POSITION="right" ID="ID_358359269" CREATED="1539519622704" MODIFIED="1539524937151">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="src_addr" POSITION="right" ID="ID_513042680" CREATED="1539519627097" MODIFIED="1539524937151">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="timestamp" POSITION="right" ID="ID_1269069840" CREATED="1539519631652" MODIFIED="1539524937151">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="ttr" POSITION="right" ID="ID_1030130175" CREATED="1539519636666" MODIFIED="1539524937152">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="type" POSITION="right" ID="ID_38167545" CREATED="1539519641878" MODIFIED="1539524937152">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="result" POSITION="right" ID="ID_868866648" CREATED="1539519665445" MODIFIED="1539524937153" BACKGROUND_COLOR="#bcb5b5">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="hop" ID="ID_614211363" CREATED="1539519669845" MODIFIED="1539524937153" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="error" ID="ID_839893807" CREATED="1539519683768" MODIFIED="1539524937153" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="result" ID="ID_1206523669" CREATED="1539519688818" MODIFIED="1539524937153">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="x" ID="ID_1425191583" CREATED="1539519760259" MODIFIED="1539524937154">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="err" ID="ID_1799197591" CREATED="1539519765281" MODIFIED="1539525078130" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="from" ID="ID_1927415454" CREATED="1539519770502" MODIFIED="1539524937155" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="ittl" ID="ID_1309517554" CREATED="1539519778279" MODIFIED="1539524937156">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="edst" ID="ID_480306873" CREATED="1539519784293" MODIFIED="1539524937156">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="late" ID="ID_668357346" CREATED="1539519792425" MODIFIED="1539524937157">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="mtu" ID="ID_468498783" CREATED="1539519797361" MODIFIED="1539524937157">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="rtt" ID="ID_240250989" CREATED="1539519803819" MODIFIED="1539524937158" BACKGROUND_COLOR="#95e096">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="size" ID="ID_42360771" CREATED="1539519810165" MODIFIED="1539524937158">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="ttl" ID="ID_1957256340" CREATED="1539519823928" MODIFIED="1539524937159">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="flags" ID="ID_204496467" CREATED="1539519831160" MODIFIED="1539524937159">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="dstoptsize" ID="ID_1551571029" CREATED="1539519836155" MODIFIED="1539524937160">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="hbhoptsize" ID="ID_781211662" CREATED="1539519841491" MODIFIED="1539524937160">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="icmpext" ID="ID_1878890540" CREATED="1539519847242" MODIFIED="1539524937160">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="version" ID="ID_1446629372" CREATED="1539519868034" MODIFIED="1539524937161">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="rfc4884" ID="ID_940758481" CREATED="1539519877868" MODIFIED="1539524937161">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="obj" ID="ID_261375555" CREATED="1539519883717" MODIFIED="1539524937161" BACKGROUND_COLOR="#bcb5b5">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="class" ID="ID_1660814703" CREATED="1539519949762" MODIFIED="1539524937162">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="type" ID="ID_33864049" CREATED="1539519955902" MODIFIED="1539524937162">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="mpls" ID="ID_1071978656" CREATED="1539519961424" MODIFIED="1539524937162">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="exp" ID="ID_673182824" CREATED="1539519980309" MODIFIED="1539524937163">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="label" ID="ID_1712560847" CREATED="1539520002643" MODIFIED="1539524937163">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="s" ID="ID_1905369696" CREATED="1539520009559" MODIFIED="1539524937164">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node TEXT="ttl" ID="ID_379799786" CREATED="1539520015928" MODIFIED="1539524937164">
+<edge COLOR="#2c1717"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
